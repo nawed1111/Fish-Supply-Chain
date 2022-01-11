38a247a9abb9eb5d843993cc4fa5f9b74b293ce6
nawed1111 <32324785+nawed1111@users.noreply.github.com>
2022-01-11 16:17:34 +0530

    Create web3.js

---
+++ bridge/web3.js
@@ -0,0 +1,16 @@
+import Web3 from "web3";
+
+let web3;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    "<paste_infura_api_url_here>"
+  );
+  web3 = new Web3(provider);
+}
+
+export default web3;
