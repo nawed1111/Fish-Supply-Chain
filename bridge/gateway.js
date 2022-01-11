decf3550f0f059e1e219bad563e7e3daae362f69
nawed1111 <32324785+nawed1111@users.noreply.github.com>
2022-01-11 16:18:11 +0530

    Update gateway.js

--- bridge/gateway.js
+++ bridge/gateway.js
@@ -2,6 +2,6 @@ import web3 from "./web3";
 import Gateway from "../build/contracts/Gateway.json";
 const instance = new web3.eth.Contract(
   Gateway.abi,
-  "0x4ce7C36b9d2ca6A594E7a688897C94B42bCa859F"
+  "<paste_deployed_contract_address_here>"
 );
 export default instance;
